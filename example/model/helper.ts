b3e8f4a8a145618cbf87fe10cd52a694dc0e988f
ArrayZoneYour <hustliyidong@gmail.com>
2018-12-06 17:09:07 +0800

    style(demo): Cleaner the demo code.

--- example/model/helper.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-const initialProviderState: ProviderProps = {}
-const GlobalContext = createContext(initialProviderState)
-const Consumer = GlobalContext.Consumer
-
-export { Consumer, GlobalContext }
