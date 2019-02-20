9c27922a9f7bea3a23ef6b549847dabda12cc66f
ArrayZoneYour <hustliyidong@gmail.com>
2019-02-20 16:28:50 +0800

    fix(type): Fix TSLint error in helper.ts.

--- src/global.ts
+++ src/global.ts
@@ -1,11 +1,11 @@
 let State: any = {}
 // Communicate between Provider-Consumer and Hooks
 // Use to provide backwards-compatible.
-let Setter = {
+let Setter: Setter = {
   // classSetter stores the setState from Provider, invoke the classSetter.setState can update the state of Global Provider.
-  classSetter: undefined as any,
+  classSetter: undefined,
   // functionSetter stores the setState returned by useStore. These setStates can invoke the rerender of hooks components.
-  functionSetter: {} as FunctionSetter
+  functionSetter: {}
 }
 
 let devTools: any
