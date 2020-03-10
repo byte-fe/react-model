f917376692bfd80d3a510e87fbb71ae9f0ce4584
ArrayZoneYour <hustliyidong@gmail.com>
2020-03-10 17:12:01 +0800

    test(coverage): add testcase to improve test coverage

--- __test__/Model/error.multi.spec.ts
+++ __test__/Model/error.multi.spec.ts
@@ -1,29 +1,31 @@
 /// <reference path="../index.d.ts" />
 import { renderHook } from '@testing-library/react-hooks'
 // @ts-ignore
-import { ErrorModel } from './errorModel'
+import { ErrorModel as EM } from './errorModel'
 import { Model } from '../../src'
 
 describe('useStore', () => {
-  test('create by single model definition', async () => {
+  test('create by single model error definition', async () => {
     let state: any
     let actions: any
     let count = 0
+    const ErrorModel = Model(EM)
     // @ts-ignore
-    const { useStore, subscribe, unsubscribe } = Model(ErrorModel)
+    const { useStore, subscribe, unsubscribe } = Model({ ErrorModel })
     renderHook(() => {
-      ;[state, actions] = useStore()
+      ;[state, actions] = useStore('ErrorModel')
     })
-    expect(state).toEqual({})
+    expect(actions).toEqual({})
     expect(actions.increment).toBe(undefined)
     // await actions.increment(3)
     expect(state).toEqual({})
     // test subscribe
+    // @ts-ignore
     subscribe('increment', () => (count += 1))
-    expect(actions).toEqual({})
     expect(count).toBe(0)
     expect(state.count).toBe(undefined)
     // test unsubscribe
+    // @ts-ignore
     unsubscribe('increment')
     expect(actions).toEqual({})
     expect(state.count).toBe(undefined)
